85c62f08c85d91d937289cdc7621b8354ddb51dc
浩之 山田 <H44bcda@gmail.com>
2021-08-10 14:34:24 +0900

    fa

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -16,23 +16,6 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
-
-        void timercall()
-        {
-            timer1.Tick += new EventHandler(timer1_Tick);
-            timer1.Interval = 100;
-            timer1.Enabled = false;
-            timer2.Tick += new EventHandler(timer2_Tick);
-            timer2.Interval = 100;
-            timer2.Enabled = false;
-            timer3.Tick += new EventHandler(timer3_Tick);
-            timer3.Interval = 100;
-            timer3.Enabled = false;
-            timer4.Tick += new EventHandler(timer4_Tick);
-            timer4.Interval = 100;
-            timer4.Enabled = false;
-        }
-
         Timer timer1 = new Timer();
         Timer timer2 = new Timer();
         Timer timer3 = new Timer();
@@ -40,40 +23,58 @@ namespace WinFormsApp1
         int x1;
         int y1;
 
+        void timercall()
+        {
+            timer1.Interval = 100;
+            timer1.Enabled = false;
+            timer2.Interval = 100;
+            timer2.Enabled = false;
+            timer3.Interval = 100;
+            timer3.Enabled = false;
+            timer4.Interval = 100;
+            timer4.Enabled = false;
+        }
+        
         private void Form1_Load(object sender, EventArgs e)
         {
             timercall();
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(Form1_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(Form1_KeyUp);
+            this.timer1.Tick += new EventHandler(timer1_Tick);
+            this.timer2.Tick += new EventHandler(timer2_Tick);
+            this.timer3.Tick += new EventHandler(timer3_Tick);
+            this.timer4.Tick += new EventHandler(timer4_Tick);
         }
-        private void timer1_Tick(object sender, EventArgs e)//「W」キーを押した時の動作
+
+        private void timer1_Tick(object sender, EventArgs e)//「W」入力時の操作
         {
             int x = pictureBox1.Location.X;
             int y = pictureBox1.Location.Y;
-            Point pt = new Point(x , y - 5);
+            Point pt = new Point(x, y - 5);
             pictureBox1.Location = pt;
         }
-        private void timer2_Tick(object sender, EventArgs e)//「A」キーを押した時の動作
+        private void timer2_Tick(object sender, EventArgs e)//「A」入力時の操作
         {
             int x = pictureBox1.Location.X;
             int y = pictureBox1.Location.Y;
-            Point pt = new Point(x - 5 , y + 0);
+            Point pt = new Point(x - 5, y + 0);
             pictureBox1.Location = pt;
         }
-        private void timer3_Tick(object sender, EventArgs e)//「S」キーを押した時の動作
+        private void timer3_Tick(object sender, EventArgs e)//「S」入力時の操作
         {
             int x = pictureBox1.Location.X;
             int y = pictureBox1.Location.Y;
-            Point pt = new Point(x , y + 5);
+            Point pt = new Point(x, y + 5);
             pictureBox1.Location = pt;
         }
-        private void timer4_Tick(object sender, EventArgs e)//「D」キーを押した時の動作
+        private void timer4_Tick(object sender , EventArgs e)//「D」入力時の操作
         {
             int x = pictureBox1.Location.X;
             int y = pictureBox1.Location.Y;
             Point pt = new Point(x + 5, y);
             pictureBox1.Location = pt;
         }
+
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.W)
